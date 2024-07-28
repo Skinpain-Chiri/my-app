bf13630ecb4cc735da9cee63605b08210a50aded
Skinpain-Chiri <chiriashraf@gmail.com>
2024-07-28 16:20:24 +0000

    Remove learn more button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,12 +7,6 @@ export default function Page() {
         <p className='mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 '>
             Never give out the same impression again 
         </p>
-        <a href='#' className='inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900'>
-            Learn more
-            <svg className='w-3.5 h-3.5 ms-2 rtl:rotate-180' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 10'>
-            <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M1 5h12m0 0L9 1m4 4L9 9'/>
-            </svg>
-        </a>
         </>
     );
 }
