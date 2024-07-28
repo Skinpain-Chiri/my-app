8c655bc1f3f9e345c5cb363b296e69a50b5156d1
chirix33 <chirixteam@gmail.com>
2024-07-28 13:57:13 +0000

    another commit

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavLinks from "@/app/ui/nav/nav-links";
+
+export default function Layout({ children } : { children: React.ReactNode }) {
+    return (
+        <>
+          <nav className='bg-white border-gray-200 dark:bg-gray-900'>
+            <NavLinks />
+          </nav>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </>
+    );
+} 
---
+++ src/app/ui/nav/nav-links.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const navLinks = [
+    {name: "Home", href: "/home"},
+    {name: "About", href: "/about"},
+    {name: "Pricing", href: "/pricing"},
+    {name: "Services", href: "/services"},
+    {name: "Contact", href: "/contact"}
+];
+
+export default function NavLinks() {
+    const pathName = usePathname();
+
+    return (
+        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
+          <img src='https://flowbite.com/docs/images/logo.svg' className='h-8' alt='Flowbite Logo' />
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>My App</span>
+        </a>
+        <div className='w-full md:block md:w-auto'>
+          <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+            {
+                navLinks.map((link) => (
+                    <li key={link.name} className='mb-3 md:mb-0'>
+                        <Link href={link.href} className=
+                        {
+                            clsx('', {
+                            'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500': pathName === link.href,
+                            'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent': pathName !== link.href
+                        })
+                        }>
+                        {link.name}
+                        </Link>
+                    </li>
+                ))
+            }
+          </ul>
+        </div>
+      </div>
+    );
+}
