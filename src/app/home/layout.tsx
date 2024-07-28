9c37f769872f45e8a7b7bc05991472451d929508
chirix33 <chirixteam@gmail.com>
2024-07-28 14:29:17 +0000

    First Commit

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children } : { children: React.ReactNode }) {
           <nav className='bg-white border-gray-200 dark:bg-gray-900'>
             <NavLinks />
           </nav>
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12 sm:mt-8">{children}</div>
         </>
     );
 } 
--- src/app/ui/nav/nav-links.tsx
+++ src/app/ui/nav/nav-links.tsx
@@ -5,10 +5,8 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
 const navLinks = [
-    {name: "Home", href: "/home"},
+    {name: "Shop", href: "/home"},
     {name: "About", href: "/about"},
-    {name: "Pricing", href: "/pricing"},
-    {name: "Services", href: "/services"},
     {name: "Contact", href: "/contact"}
 ];
 
@@ -18,8 +16,7 @@ export default function NavLinks() {
     return (
         <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
         <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
-          <img src='https://flowbite.com/docs/images/logo.svg' className='h-8' alt='Flowbite Logo' />
-          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>My App</span>
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>Perfumes</span>
         </a>
         <div className='w-full md:block md:w-auto'>
           <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
