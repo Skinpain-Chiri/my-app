5d13341457624f522fa4f88562d18594cca3fa4d
Skinpain-Chiri <chiriashraf@gmail.com>
2024-07-28 19:49:47 +0000

    update data

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,19 +2,14 @@ import { sql } from "@vercel/postgres";
 import { Perfume } from "./type";
 
 export async function getPerfumes() {
-    try {
-        const result = await sql<Perfume>`SELECT * FROM perfumes`;
-        const perfumes = result.rows.map((perfume) => {
-            return {
-                ...perfume,
-                prize: perfume.prize.toFixed(2)
-            }
-        });
+    const result = await sql<Perfume>`SELECT * FROM perfumes`;
+    const perfumes = result.rows.map((perfume) => {
+        return {
+            ...perfume,
+            prize: perfume.prize.toFixed(2)
+        }
+    });
 
-        await Promise.all(perfumes);
-        return perfumes;
-    } catch (error: any) {
-        console.log(error);
-        return [];
-    }
+    await Promise.all(perfumes);
+    return perfumes;
 }
